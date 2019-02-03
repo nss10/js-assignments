8abffe425ac96a58c05fc0cbc27a92a6486d6d96
nss10 <nss10@outlook.com>
2019-02-04 00:25:35 +0530

    Completed the task partially. Leaving only the last one

--- task/08-objects-tasks.js
+++ task/08-objects-tasks.js
@@ -23,8 +23,14 @@
  *    console.log(r.getArea());   // => 200
  */
 function Rectangle(width, height) {
-    throw new Error('Not implemented');
+    this.width = width;
+    this.height = height;
+    
 }
+
+Rectangle.prototype.getArea = function(){
+    return this.width * this.height;
+};
 
 
 /**
@@ -38,7 +44,7 @@ function Rectangle(width, height) {
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
 function getJSON(obj) {
-    throw new Error('Not implemented');
+    return JSON.stringify(obj);
 }
 
 
@@ -54,7 +60,14 @@ function getJSON(obj) {
  *
  */
 function fromJSON(proto, json) {
-    throw new Error('Not implemented');
+    let obj = Object.create(proto);
+    let newObj = JSON.parse(json);
+    for (const key in newObj) {
+        if (newObj.hasOwnProperty(key)) {
+            obj[key] = newObj[key];
+        }
+    }
+    return obj;
 }
 
 
